6e5f455b1f7de07a556e71cf885cfa92470f7aaf
ABA Games <cs8k-cyu@asahi-net.or.jp>
2020-04-18 18:05:14 +0900

    Shorten code

--- docs/lland/main.js
+++ docs/lland/main.js
@@ -9,6 +9,7 @@ characters = [];
 options = {
   isPlayingBgm: true,
   isReplayEnabled: true,
+  isMinifying: true,
 };
 
 let mountains;
@@ -17,19 +18,17 @@ let offset;
 let mountainAppDist;
 let mountainIndex;
 let landingIndex;
-let lowMountainIndex;
 let landing;
 let landZ;
 let shipCollision;
 let m;
 
 function update() {
-  if (ticks === 0) {
+  if (!ticks) {
     mountains = times(9, (i) => {
       return { y: 90 - i, c: "red" };
     });
-    shipY = 9;
-    shipV = offset = mountainAppDist = mountainIndex = landingIndex = lowMountainIndex = landing = 0;
+    shipY = shipV = offset = mountainAppDist = mountainIndex = landingIndex = landing = 0;
   }
   mountains.map((m, i) => {
     color(m.c);
@@ -37,7 +36,7 @@ function update() {
   });
   color("green");
   shipCollision = box(25, shipY, 5, 5);
-  if (landing > 0) {
+  if (landing) {
     if (input.isPressed) {
       landing = 0;
     } else {
@@ -45,23 +44,18 @@ function update() {
     }
   }
   offset -= difficulty;
-  mountainAppDist -= difficulty;
-  if (mountainAppDist < 0) {
+  if ((mountainAppDist -= difficulty) < 0) {
     m = mountains[wrap(mountainIndex, 0, 9)];
     m.y =
-      lowMountainIndex > 0
-        ? 90
-        : landingIndex === 1
+      landingIndex > 7 || landingIndex === 1
         ? rnd(70, 90)
         : landingIndex === 0
         ? (landZ = rnd(40, 70))
         : rnd(40, 90);
     landingIndex--;
-    lowMountainIndex--;
     if (landingIndex < 0) {
       m.c = "cyan";
-      landingIndex = rndi(9, 12);
-      lowMountainIndex = 2;
+      landingIndex = 9;
     } else {
       m.c = "red";
     }
@@ -83,17 +77,15 @@ function update() {
   shipY += shipV * difficulty;
   if (shipCollision.isColliding.rect.cyan) {
     play("select");
-    score++;
-    landing = 1;
+    landing = ++score;
     shipV = 0;
     shipY = landZ - 3;
     mountains.map((n) => (n.c = "red"));
-  } else if (shipCollision.isColliding.rect.red) {
-    play("explosion");
-    end();
   }
-  color("transparent");
-  if (rect(0, 0, 1, 99).isColliding.rect.cyan) {
+  if (
+    shipCollision.isColliding.rect.red ||
+    rect(-1, 0, 1, 99).isColliding.rect.cyan
+  ) {
     play("explosion");
     end();
   }
