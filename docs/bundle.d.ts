68b267375f12817abb47ffb26b5095c5c87664e9
ABA Games <cs8k-cyu@asahi-net.or.jp>
2022-08-25 20:40:51 +0900

    Update type definition

--- docs/bundle.d.ts
+++ docs/bundle.d.ts
@@ -487,80 +487,147 @@ declare type SoundEffectType =
   | "jump"
   | "select"
   | "lucky"
-  | "random";
-declare function play(type: SoundEffectType): void;
+  | "random"
+  | "click"
+  | "synth"
+  | "tone";
+declare function play(
+  type: SoundEffectType,
+  options?: {
+    seed?: number;
+    numberOfSounds?: number;
+    volume?: number;
+    pitch?: number;
+    freq?: number;
+    note?: string;
+  }
+): void;
+declare function playBgm(): void;
+declare function stopBgm(): void;
 
 // sounds-some-sounds interface
 // https://github.com/abagames/sounds-some-sounds
 declare module sss {
-  // Play the sound effect
-  function play(
-    name?: string,
-    numberOfSounds?: number,
-    pitch?: number,
-    volume?: number
-  ): void;
+  // Play sound effect
   function playSoundEffect(
-    type?:
-      | "coin"
-      | "laser"
-      | "explosion"
-      | "powerUp"
-      | "hit"
-      | "jump"
-      | "select"
-      | "random"
-      | "synth"
-      | "tone"
-      | "click",
-    _options?: {
+    // The list of SoundEffectType is as follows:
+    // "coin", "laser", "explosion", "powerUp", "hit", "jump", "select",
+    // "random"("lucky"), "click", "synth", "tone"
+    type: SoundEffectType,
+    options?: {
+      // Random seed (default = 0)
       seed?: number;
+      // Number of simultaneous sounds (default = 2)
       numberOfSounds?: number;
-      pitch?: number;
+      // Sound volume (default = 1)
       volume?: number;
+      // To set the pitch of the sound, set one of the following 3 parameters
+      pitch?: number; // MIDI note number
+      freq?: number; // Frequency (Hz)
+      note?: string; // Note string (e.g. "C4", "F#3", "Ab5")
     }
-  ): void;
-  // Play generated background music
-  function playBgm(
-    name?: string,
-    pitch?: number,
-    len?: number,
-    interval?: number,
-    numberOfTracks?: number,
-    soundEffectTypes?: string[],
-    volume?: number
-  ): void;
-  // Stop the background music
-  function stopBgm(): void;
-  // Play generated jingle
-  function playJingle(
-    name?: string,
-    isSoundEffect?: boolean,
-    note?: number,
-    len?: number,
-    interval?: number,
-    numberOfTracks?: number,
-    volume?: number
-  ): void;
-  // Stop all jingles
-  function stopJingles(): void;
+  ): SoundEffect;
   // Play music described in MML
   function playMml(
     mmlStrings: string[],
-    _options?: { volume?: number; speed?: number; isLooping?: boolean }
-  ): void;
+    options?: {
+      // Sound volume (default = 1)
+      volume?: number;
+      // Playback speed (default = 1)
+      speed?: number;
+      // Looping at the end of the music (default = true)
+      isLooping?: boolean;
+    }
+  ): Track;
   // Stop MML music
-  function stopMml(): void;
+  function stopMml(track?: Track): void;
+  // Generate mml strings
+  function generateMml(options?: {
+    // Random seed (default = 0)
+    seed?: number;
+    // Generated music length (16 notes = 1 bar) (default = 32)
+    noteLength?: number;
+    // Number of simultaneous parts (default = 4)
+    partCount?: number;
+    // Probability of drum part generation (default = 0.5)
+    drumPartRatio?: number;
+  }): string[];
+  // Initialize the library
+  function init(
+    // Used to generate sound effects and MMLs
+    baseRandomSeed?: number,
+    // When reusing an existing AudioContext
+    audioContext?: AudioContext
+  ): void;
+  // The startAudio function needs to be called from within
+  // the user operation event handler to enable audio playback in the browser
+  function startAudio(): void;
+  // The update function needs to be called every
+  // certain amount of time (typically 60 times per second)
+  function update(): void;
   // Set the tempo of the music
   function setTempo(tempo?: number): void;
   // Set the quantize timing of sound effects by the length of the note
   function setQuantize(noteLength?: number): void;
   // Set a master volume
   function setVolume(volume?: number): void;
-  // Rest all states
+  // Reset all states
   function reset(): void;
-  // Set a random number seed
+  // Set a random seed number
   function setSeed(baseRandomSeed?: number): void;
+
+  type SoundEffectType =
+    | "coin"
+    | "laser"
+    | "explosion"
+    | "powerUp"
+    | "hit"
+    | "jump"
+    | "select"
+    | "lucky"
+    | "random"
+    | "click"
+    | "synth"
+    | "tone";
+
+  type SoundEffect = {
+    type: SoundEffectType;
+    params;
+    volume: number;
+    buffers: AudioBuffer[];
+    bufferSourceNodes: AudioBufferSourceNode[];
+    gainNode: GainNode;
+    isPlaying: boolean;
+    playedTime: number;
+    isDrum?: boolean;
+    seed?: number;
+  };
+
+  type Note = {
+    pitch: number;
+    quantizedStartStep: number;
+    quantizedEndStep: number;
+  };
+
+  type Part = {
+    mml: string;
+    sequence: { notes: Note[] };
+    soundEffect: SoundEffect;
+    noteIndex: number;
+    endStep: number;
+    visualizer?;
+  };
+
+  type Track = {
+    parts: Part[];
+    notesStepsCount: number;
+    notesStepsIndex: number;
+    noteInterval: number;
+    nextNotesTime: number;
+    speedRatio: number;
+    isPlaying: boolean;
+    isLooping: boolean;
+  };
 }
 
 // Button
